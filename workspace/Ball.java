bad18e09f0bc503e5f92b18097f6f51ac1b12265
El00777 <elnatandemelash76@gmail.com>
2024-12-13 18:34:23 +0000

    commit -m"submission 12/13"

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -2,12 +2,67 @@ import java.awt.*;
 
 public class Ball {
 	//your code here!
+	private int x;
+	private  int y;
+	private int size;
+	private int xVelocity;
+	private int yVelocity;
 	
 	//don't forget you need instance variables:
 	
 	
 	//constructor(s):
-	
-	//methods:
+	public Ball (int x,int y, int size ){
+		this.x=x;
+		this.y=y;
+		this.size= size;
+		xVelocity=1;
+		yVelocity=1;
 
+	}
+	public void draw(Graphics g){
+		g.setColor(Color.blue);
+		g.fillOval(x, y, size, size);
+	}
+
+	public int  getYpos(){
+		return y;
+
+	}
+	public int  getXpos(){
+		return x;
+
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setX(int newX){
+		x = newX;
+	}
+	
+	public void move(){
+		x += xVelocity;
+		y += yVelocity;
+	}
+  // Reverse the X direction
+  public void reverseX() {
+	xVelocity = -xVelocity;
 }
+
+// Reverse the Y direction
+public void reverseY() {
+	yVelocity = -yVelocity;
+}
+public void setXVelocity(int xVelocity) {
+	this.xVelocity = xVelocity;
+}
+
+public void setYVelocity(int yVelocity) {
+	this.yVelocity = yVelocity;
+}
+public void setY(int digit){
+	this.y= digit;
+}
+	
+}
+
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,4 +1,3 @@
-import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
@@ -6,11 +5,61 @@ public class Paddle {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
-	
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int velocity;
+
+
+
+
 	//constructor(s):
 	
-	//methods:
-	
-	
+	public Paddle(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.velocity = 0;  
+    }
+
+    // Methods
+    public void addVelocity(int amount) {
+        velocity += amount; 
+    }
+    public void draw(Graphics g) {
+        g.setColor(Color.red);
+		g.fillRect(x, y, width, height);
+    }
+
+
+	public void setX(int newX){
+		x = newX;
+	}
+
+    public int getX() {
+		return x;
+    }
+	public int getY() {
+		return y;
+    }
+	public int getWidth(){
+		return width;
+	}
+	public int getHeight() {
+		return height;
+    }
+	public int getVelocity(){
+		return velocity;
+	}
+	public void move (){
+		x+=0;
+		y+=0;
+	} 
+public void setVelocity(int speed) {
+	velocity = speed; 
 }
+
+    }
+
